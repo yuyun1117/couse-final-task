9a7310d5e94858de97f98baa7661029e9b73f983
linwenjun <linwenjun@163.com>
2018-06-08 15:48:16 +0800

    modify some config

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const _ = require('lodash')
+const jwt = require('jsonwebtoken');
+
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!!!')
+app.get('/token', function (req, res) {
+  res.send(jwt.sign({"name": "zhangsan"}, "ccc"))
+})
+
+app.get('*', function (req, res) {
+  const token = req.get('Authorization').replace(/^Bearer\s/, '');
+  jwt.verify(token, "ThisIsMySecret", {algorithms: ["HS256"]}, (err, decoded)=> {
+    res.send(JSON.parse(decoded.sub));
+  })
 })
 
 app.listen(3000, function () {
--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 ```
 npm i
 npm start
+
+# 下载 pm2 依赖
+npm i -D pm2
+
 ```
