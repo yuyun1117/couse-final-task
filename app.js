a0d2b82dddfc28eb7743752bc8eba33de8ebe85a
yuyun1117 <yuyun_1117@sina.com>
2018-06-13 18:17:52 +0800

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const pug = require('pug')
 
 const app = express()
 
+let rawList = [
+  {id: 1, name: "数据库系统与原理"},
+  {id: 2, name: "软件测试       "},
+  {id: 3, name: "软件过程管理   "}
+]
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
@@ -10,15 +16,43 @@ app.get('/token', function (req, res) {
   res.send("token");
 })
 
+app.get('/delete/:id', function(req, res) {
+  
+  let id = parseInt(req.params.id);
+  //console.log('++++',req.params)
+  rawList = rawList.filter(function(data){
+    return data.id !== id
+  })
+  res.redirect("/list")
+})
+
+app.get('/edit/:id', function(req, res) {
+  
+  let id = parseInt(req.params.id);
+  //console.log('++++',req.params)
+  rawList = rawList.filter(function(data){
+    return data.id == id
+  })
+  res.redirect("/list")
+})
+
+
+app.get('/list', function(req, res) {
+  res.render('list', {list: rawList})
+})
+
+/**
 app.get('/list', function(req, res) {
   res.render('list', {list: [
     {id: 1, name: "zzz"},
-    {id: 2, name: "xxx"}
+    {id: 2, name: "xxx"},
+    {id: 3, name: "yyy"}
   ]})
-})
+})**/
 
 app.get('*', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
+  
 })
 
 app.listen(3000, function () {
--- readme.md
+++ readme.md
@@ -1,18 +1,7 @@
-### 运行
-```
-npm i
-npm start
-npm restart
-npm stop
-```
-
-```sh
-# 查看指定端口的占用情况
-C:\>netstat -aon|findstr "9050"
-
-# 查看PID对应的进程
-C:\>tasklist|findstr "2016"
-
-# 结束该进程
-C:\>taskkill /f /t /im tor.exe
-```
+余韵 3117370079  https://github.com/yuyun1117
+李萱 3117370064  https://github.com/lixuan1 
+韩琦 3117370059  https://github.com/Hanqiqi
+项蕾 3117370058  https://github.com/xianglei96
+石晓霞 3117370083  https://github.com/Shawya-Sxx
+戴梦瑶 3117370057 https://github.com/DaiMengyao
+刘慧敏 3117370063 https://github.com/liuhuimin0063
